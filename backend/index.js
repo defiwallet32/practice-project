2b6524b3bd9ecb69fc250ed3de07fc2fbeaada64
Rahul Sharma <shubhamrushc@gmail.com>
2025-05-06 15:33:04 +0530

    first comment

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+const { v4: uuidv4 } = require("uuid");
+
+const app = express();
+app.use(cors());
+
+app.get("/generate-link", (req, res) => {
+  const id = uuidv4();
+  res.send(`http://localhost:3000/?id=${id}`);
+});
+
+app.listen(5000, () => {
+  console.log("Backend running on http://localhost:5000");
+});
